9d789e7ace3c0f545ab950e7536103ac8ecf6d3d
mihali-marioara-30121 <marioaramihali2002@gmail.com>
2023-11-05 17:45:20 +0200

    login works

--- SCD_Frontend/Form1.cs
+++ SCD_Frontend/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SCD_Frontend.services;
+using System;
 using System.Windows.Forms;
 
 namespace SCD_Frontend
@@ -18,6 +19,20 @@ namespace SCD_Frontend
             LoginService loginService = new LoginService();
             String authResponse = loginService.SendAuthenticatedRequest(username, password);
             Console.WriteLine(authResponse);
+            String role = null;
+            string securityKey = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
+            if (authResponse != "Authentication failed") {
+                 role = JwtDecoderService.GetRoleFromJwt(authResponse);
+            }
+
+            if (authResponse == "Authentication failed" || role != "ADMIN")
+            {
+                MessageBox.Show("Authentication failed");
+            } else
+            {
+                MessageBox.Show("Authentication sucessfull");
+            }
+
         }
     }
 }
---
+++ SCD_Frontend/services/JwtDecoderService.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SCD_Frontend.services
+{
+    internal class JwtDecoderService
+    {
+        public static string GetRoleFromJwt(string jwtToken)
+        {
+            string base64encodedPayload = jwtToken.Split('.')[1];
+
+            // Replace Base64 URL-specific characters
+            base64encodedPayload = base64encodedPayload.Replace('-', '+').Replace('_', '/');
+            string decodedString = null;
+
+            try
+            {
+                // Add padding if necessary
+                int padding = base64encodedPayload.Length % 4;
+                if (padding > 0)
+                {
+                    base64encodedPayload += new string('=', 4 - padding);
+                }
+
+                byte[] bytes = Convert.FromBase64String(base64encodedPayload);
+                decodedString = Encoding.UTF8.GetString(bytes);
+                Console.WriteLine("Decoded payload: " + decodedString);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid Base64-encoded payload.");
+            }
+
+            try
+            {
+                using (JsonDocument doc = JsonDocument.Parse(decodedString))
+                {
+                    JsonElement root = doc.RootElement;
+
+                    if (root.TryGetProperty("role", out JsonElement roleElement) && roleElement.ValueKind == JsonValueKind.String)
+                    {
+                        return roleElement.GetString();
+
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+            } catch (Exception ex) { return null; }
+        }
+    }
+}
